40e4ce0d557bf9d41c6566b87e5f0fb55c3d6ed1
lvsz <lvsuzhen@zhenmeilideMacBook-Pro.local>
2018-04-26 22:27:16 +0800

    增加动效

--- index.js
+++ index.js
@@ -16,16 +16,48 @@ for (let i = 0; i < oLi.length; i++) {
     }
 }
 //页面导航定位
-let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+
+let aTags = document.querySelectorAll('nav.menu > ul > li > a');
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
    for (let i = 0; i < aTags.length; i++) {
        aTags[i].onclick = function(e) {
             e.preventDefault()
             let aId = this.getAttribute('href');
             if (!(aId === 'javascript:;')) {
-                let elem = document.querySelector(aId),
-                  elemTop = elem.offsetTop; 
-                  console.log(elemTop)
-                  window.scrollTo(0,elemTop-80)    
+                let elem = document.querySelector(aId);
+               // let n = 25; //1s以内一共动多少次
+               // let duration = 500 / 25; //每次花费多少时间
+                var targetTop = elem.offsetTop - 80;//目标距离
+                var currTop = window.scrollY;//当前距离
+                var distance = (targetTop - currTop);
+                // var i = 0;
+                // console.log(targetTop)
+                // console.log(currTop)
+                // console.log(distance)
+                // var timer =  setInterval(function() {
+                //         i+=1;
+                //         if ( i > 25) {
+                //             clearInterval(timer);
+                //             return;
+                //         }
+                //         window.scrollTo(0,currTop + distance * i)
+                //    },duration) 
+                var coords = { y: currTop}; // 起始位置
+                var t = Math.abs((distance/100)*100) // Math.abs()方法是取绝对值  这里的时间是根据距离来算的，算出距离是100像素的几倍 再乘以100ms 就是几百毫秒的时间
+                if(t>500){ t = 500 }// 如果时间太长就直接设置为500ms
+                var tween = new TWEEN.Tween(coords) // 起始位置
+                            .to({ y: targetTop}, t) // 结束位置 和 时间 
+                            .easing(TWEEN.Easing.Cubic.InOut) // 缓动类型
+                            .onUpdate(function() {
+                                // coords.y 已经变了
+                                window.scrollTo(0,coords.y) // 如何更新界面
+                            })
+                            .start(); // 开始缓动
+            
           }
        }
    }
